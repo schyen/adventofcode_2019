02e4754b377e4cd31bfa412e78069b5b38ce4089
syen <syen@kgen1.(none)>
2019-12-12 17:03:18 +0000

    wip on day 3 puzzle 2

--- day03.py
+++ day03.py
@@ -12,15 +12,20 @@ wires = open("day03.txt", "r").readlines()
 
 # put each wire into a list -- panel is list of wires
 panel = []
+delta = []
 for w in wires:
     
     wire = w.strip('\n').split(',')
-    
-    # initialize coordinates with origin
+    print(wire[0:5])
+    # store steps as lines
     lines = []
+
+    # initialize distance taken with each line
+    wire_delta = []
     
     # going through one coordinate at a time
     for i, step in enumerate(wire):
+        # initialize coordinates with origin
         if i == 0:
             ref_coord = (0,0)
         else:
@@ -60,58 +65,55 @@ for w in wires:
         # create pairs of coordinates
         coord_pair = (ref_coord, entry_coord)
         lines.append(coord_pair)
-        # # create coordinates for all points between ref coord and entry coord
-        # if axis == 'x':
-        #     dum_x = list(range(ref_coord[0], entry_coord[0], step_dir))
-        #     dum_y = np.repeat(y_val, len(dum_x))
-        # else:
-        #     dum_y = list(range(ref_coord[1], entry_coord[1], step_dir))
-        #     dum_x = np.repeat(x_val, len(dum_y))
         
-        # for x, y in zip(dum_x, dum_y):
-        #     entry = (x, y)
-        #     coord.append(entry)        
+        # record number of steps taken with each line
+        wire_delta.append(magnitude)
 
     # drop origin
     del lines[0]
 
     # convert coordinate pairs into lines
     entry  = MultiLineString(lines)
-    
+    print(wire_delta[0:5])
     # panel is list of wires
-    panel.append(entry) 
+    panel.append(entry)
+    delta.append(wire_delta)
+
 
 print(len(panel[0]))
 print(len(panel[1]))
 
 # find blue lines that intersect with red lines
 keep = []
-for red in panel[0]:
-    for blue in panel[1]:
+red_line = []
+blue_line = []
+xpoint = []
+x_index = []
+
+for r_i, red in enumerate(panel[0]):
+    for b_i, blue in enumerate(panel[1]):
         # find when wires intersect
         check = blue.intersects(red)
         if check:
             # store interesecting wires as tuples 
             keep.append((red, blue))
 
-print(len(keep))
+            # extract line coordinates corresponding to intersecting lines
+            red_coord = (red.coords[0], red.coords[1])
+            blue_coord = (blue.coords[0], blue.coords[1])
+            red_line.append(red_coord)
+            blue_line.append(blue_coord)
 
-# extract line coordinates
-red_line = []
-blue_line = []
-for pair in keep:
-    red = pair[0]
-    blue = pair[1]
-    red_coord = (red.coords[0], red.coords[1])
-    blue_coord = (blue.coords[0], blue.coords[1])
-    red_line.append(red_coord)
-    blue_line.append(blue_coord)
+            # index of lines that intersect
+            x_index.append((r_i, b_i))
+            
+            # find points of intersection
+            entry = red.intersection(blue)
+            entry = list(entry.coords)
+            xpoint.append(entry)
 
-# find points of intersection
-xpoint = []
-for i, pair in enumerate(keep):
-    entry = pair[0].intersection(pair[1])
-    xpoint.append(entry)
+# flatten list of intersection coordinates
+xpoint = [i for sublist in xpoint for i in sublist]
 
 # calculate manhattan's distance of intersections to origin
 origin = (0,0)
@@ -119,7 +121,49 @@ mdist = []
 for x in xpoint: 
     entry = distance.cityblock(origin, x)
     mdist.append(entry)
-print(mdist)
+#print(mdist)
+
 # intersection closest to origin
 answer = min(mdist)
+#print(answer)
+
+# count steps taken by each wire to get to each intersection
+x_dist = []
+for i, x  in enumerate(x_index):
+
+    # i = ith intersection
+    # x = index of (red, blue) wires that intersected
+
+    # number of steps taken to get to intersection
+    r_index = x[0] - 1 # subtract 1 because intersection is likely in middle of a line
+    b_index = x[1] - 1
+
+    # distance wire travelled to get to intersection (less one line)
+    r_dist = sum(delta[0][0:r_index])
+    b_dist = sum(delta[1][0:b_index])
+
+    # need to account for distance to intersection on the last line
+    # coordinates of last line
+    r_last = red_line[i][0]
+    b_last = blue_line[i][0]
+
+    # calculate distance from beginning of last line to intersection
+    for x,y in zip(r_last, xpoint[i]):
+        check = x - y
+        if check > 0:
+            r_adjust = check
+            
+    for x,y in zip(b_last, xpoint[i]):
+        check = x - y
+        if check > 0:
+            b_adjust = check
+    
+    # record total distance both wires took to get to intersection
+    entry = r_dist + r_adjust + b_dist + b_adjust
+    x_dist.append(entry)
+
+print(x_dist)
+
+answer = min(x_dist)
 print(answer)
+    
