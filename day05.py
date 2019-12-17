d9e066c5214658fa43068536a068c2beb7e02f87
syen <syen@kgen1.(none)>
2019-12-17 11:33:38 +0000

    day 05 puzzle 1 attempt 1

--- day05.py
+++ day05.py
@@ -1,4 +1,11 @@
 # day 5
 
-# puzzle 1
+# import modules
+import numpy as np
+from opcode import my_program
 
+# read in input data
+raw = np.loadtxt(fname = "day05.txt", delimiter = ',')
+
+# puzzle 1
+myprogram(raw, verbose = True)
--- opcode.py
+++ opcode.py
@@ -1,36 +1,48 @@
 # opcode for advent of code 2019
 # init_prog is a list of numbers
 
+import sys
+import itertools
+
 # noun, verb, and final value for address 0 are specified in param
-def my_program(init_prog, noun=12, verb=2, verbose=False):
+def my_program(init_prog, noun=None, verb=None, verbose=False):
     
     # first copy over initial prog
     prog = init_prog
-    
-    # changing elements 1 and 2
-    index = [1, 2]
-    replacement = [noun, verb]
-    for (i, r) in zip(index, replacement):
-        prog[i] = r
+
+    if noun != None & verb != None:
+        
+        # changing elements 1 and 2
+        index = [1, 2]
+        replacement = [noun, verb]
+        for (i, r) in zip(index, replacement):
+            prog[i] = r
 
     # evaluate first opcode and parameters
     pos = 0
-
+    output = []
+    iternum = 1
     run = TRUE
     # going through program at specified positions
     while run:
-
+        print(iternum)
+        print(pos)
         # seperate out opcode from parameter
         instruction = [int(d) for d in map(str, prog[pos])]
 
         ## opcode always last  2 digits
         opcode = instruction[-2:]
         ## mode are all digits prior to opcode
-        mode = instruction[:(len(instruction)-len(opcode)+1)]
-        ## if opcode 1 or 2, pad with leading 0 in mode for the 'write instruction'
+        mode = instruction[:(len(instruction)-len(opcode))]
+        ## if opcode 1 or 2, pad with leading 0 in mode for the 'write instructio'
         if opcode in [1,2]:
+            mode_len = len(mode)
+            mode = list(itertools.repeat(0,  3-mode_len)) + mode  
+        ## for other opcodes, pad with one 0 if no modes detected
+        elif len(mode) < 1:
             mode = [0] + mode
-            
+
+        print(mode)
         # run through program sequence by the number of parameters
         step = len(opcode) + len(mode)
 
@@ -69,6 +81,7 @@ def my_program(init_prog, noun=12, verb=2, verbose=False):
 
         # interperate opcode to determine output value and write position
         if opcode == 99:
+            print('program halted')
             run = False # halt program
         elif opcode == 1:
             result = par_val[0] + par_val[1]
@@ -77,26 +90,35 @@ def my_program(init_prog, noun=12, verb=2, verbose=False):
             result = par_val[0] * par_val[1]
             r_pos = par_val[2]
         elif opcode == 3:
-            result = result # result is same result as prev itereration
+            result = int(input("Enter a number: "))
             r_pos = par_val[0]
         elif opcode == 4:
             result = par_val[0]
-            r_pos = par_val[0]
-            
+            r_pos = None
         else:
             print('unknown opcode')
             result = np.nan
             continue
 
-        # write result to position
-        prog[r_pos] = result
+        # write result to position or return a result
+        if r_pos is None:
+            print('test result')
+            output.append(result)
+            print(result)
+        else:
+            prog[r_pos] = result
+
+        # get position of next set of instructions
+        pos = pos + step
         
         # verbose
-        verb_rowname = ['prog','x','y','result']
-        verb_colname = ['position','value']
-        mat = np.reshape((pos, x_pos, y_pos, r_pos, opcode, x, y, result), (2,4))
-        check = pd.DataFrame(mat, verb_colname, verb_rowname)
         if verbose:
-            print(check)
-    
-    return prog, noun, verb
+            print("opcode: %d" % opcode)
+            print("param mode:", end = " ")
+            print(mode)
+            print("param value:", end = " ")
+            print(par_val)
+            print("result: %d" % result)
+
+        iternum += 1
+    return prog, internum, output
