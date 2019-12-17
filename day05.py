5659c1d73f0f710095d99885f2639d74a99fa088
syen <syen@kgen1.(none)>
2019-12-17 15:11:29 +0000

    day 5 puzzle 1 solved

--- day05.py
+++ day05.py
@@ -1,11 +1,19 @@
 # day 5
 
 # import modules
-import numpy as np
 from opcode import my_program
 
 # read in input data
-raw = np.loadtxt(fname = "day05.txt", delimiter = ',')
+#raw = np.loadtxt(fname = "day05.txt", delimiter = ',')
+with open("day05.txt") as f:
+    for line in f:
+        raw = line.strip('\n').split(',')
 
+raw = [int(x) for x in raw]
+print(raw)
+print(len(raw))
 # puzzle 1
-myprogram(raw, verbose = True)
+result, iternum, answer = my_program(raw, verbose = False)
+
+print(answer)
+print(result[0])
--- opcode.py
+++ opcode.py
@@ -10,7 +10,7 @@ def my_program(init_prog, noun=None, verb=None, verbose=False):
     # first copy over initial prog
     prog = init_prog
 
-    if noun != None & verb != None:
+    if noun != None and verb != None:
         
         # changing elements 1 and 2
         index = [1, 2]
@@ -22,18 +22,29 @@ def my_program(init_prog, noun=None, verb=None, verbose=False):
     pos = 0
     output = []
     iternum = 1
-    run = TRUE
+    run = True
     # going through program at specified positions
     while run:
-        print(iternum)
-        print(pos)
+        print("--------------------iter num: %d" % iternum)
+        print("pos: %d" % pos)
         # seperate out opcode from parameter
-        instruction = [int(d) for d in map(str, prog[pos])]
+        instruction = [int(d) for d in str(prog[pos])]
 
         ## opcode always last  2 digits
         opcode = instruction[-2:]
+        
         ## mode are all digits prior to opcode
         mode = instruction[:(len(instruction)-len(opcode))]
+
+        # convert opcode to integer
+        opcode = int(''.join(map(str, opcode)))
+
+        # check for halt program
+        if opcode == 99:
+            print('program halted')
+            run = False # halt program
+            break
+            
         ## if opcode 1 or 2, pad with leading 0 in mode for the 'write instructio'
         if opcode in [1,2]:
             mode_len = len(mode)
@@ -42,18 +53,25 @@ def my_program(init_prog, noun=None, verb=None, verbose=False):
         elif len(mode) < 1:
             mode = [0] + mode
 
-        print(mode)
-        # run through program sequence by the number of parameters
-        step = len(opcode) + len(mode)
-
-        # convert opcode to integer
-        opcode = int(''.join(map(str, opcode)))
+        # mode is in reverse order
+        mode = list(reversed(mode))
         
+        # run through program sequence by the number of parameters
+        step = len(mode)
+ 
         # extract parameters positions
         par_pos = [i+pos+1 for i,x in enumerate(mode)]
 
         # reading parameters
         par = [prog[i] for i in par_pos]
+        if verbose:
+            print("instruction:", end = " ")
+            print(instruction)
+            print("opcode: %d" % opcode)
+            print("param:", end=" ")
+            print(par)
+            print("param mode:", end = " ")
+            print(mode)
         
         # put paramter and mode together as tuple
         par_tuple = []
@@ -71,7 +89,7 @@ def my_program(init_prog, noun=None, verb=None, verbose=False):
             # position mode
             if curr_mode == 0:
                 # positions of value based on parameter
-                curr_val = int(prog[curr_par])
+                curr_val = prog[curr_par]
             elif curr_mode == 1:
                 # value is the parameter
                 curr_val = curr_par
@@ -80,45 +98,42 @@ def my_program(init_prog, noun=None, verb=None, verbose=False):
             par_val.append(curr_val)
 
         # interperate opcode to determine output value and write position
-        if opcode == 99:
-            print('program halted')
-            run = False # halt program
-        elif opcode == 1:
+        if opcode == 1:
             result = par_val[0] + par_val[1]
-            r_pos = par_val[2]
+            r_pos = par_tuple[2][1]
         elif opcode == 2:
             result = par_val[0] * par_val[1]
-            r_pos = par_val[2]
+            r_pos = par_tuple[2][1]
         elif opcode == 3:
             result = int(input("Enter a number: "))
-            r_pos = par_val[0]
+            r_pos = par_tuple[0][1]
         elif opcode == 4:
-            result = par_val[0]
-            r_pos = None
+            print('*******************************')
+            print('test result')
+           
+            result = prog[par_tuple[0][1]]
+            r_pos = 0
+
+            output.append(result)
+            print(result)
+
         else:
             print('unknown opcode')
             result = np.nan
             continue
-
-        # write result to position or return a result
-        if r_pos is None:
-            print('test result')
-            output.append(result)
-            print(result)
-        else:
-            prog[r_pos] = result
-
-        # get position of next set of instructions
-        pos = pos + step
-        
-        # verbose
+                # verbose
         if verbose:
-            print("opcode: %d" % opcode)
-            print("param mode:", end = " ")
-            print(mode)
             print("param value:", end = " ")
             print(par_val)
             print("result: %d" % result)
+            print("r_pos: %d" % r_pos)
 
+        # write result to position or return a result
+        if r_pos is not None:
+            prog[r_pos] = result
+
+        # get position of next set of instructions
+        pos = pos + step + 1
+        
         iternum += 1
-    return prog, internum, output
+    return prog, iternum, output
